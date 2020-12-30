5bf468aafdb2fdbd0081dc4d27515d191bd4e43a
Anton Komissarov <komissarov1988@gmail.com>
2020-12-30 22:51:35 +0500

    chat window, message text field and send button

--- src/hw4/Controller.java
+++ src/hw4/Controller.java
@@ -1,4 +1,31 @@
 package hw4;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 public class Controller {
+    @FXML
+    public TextArea chat;
+    @FXML
+    public TextField messageField;
+
+    @FXML
+    public void sendMessage() {
+        if (messageField.getLength() == 0) {
+            messageField.requestFocus();
+            return;
+        }
+        chat.appendText(messageField.getText() + "\n");
+        messageField.clear();
+        messageField.requestFocus();
+    }
+
+    @FXML
+    public void onKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode().getCode() == 10) {
+            sendMessage();
+        }
+    }
 }
